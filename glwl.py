85f5c6957f5f18ce006d1769d370aef4baa652f4
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-04-05 15:34:02 -0400

    Add anki output format option

--- glwl.py
+++ glwl.py
@@ -9,6 +9,8 @@ import requests
 from pprint import pprint
 from google.cloud import translate_v2 as translate
 
+# TODO: test out DWDS API - https://www.dwds.de/d/api
+
 parser = argparse.ArgumentParser(
         prog="glwl",
         description="German language lookup script",
@@ -16,7 +18,7 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('text', help="Word to lookup")
 parser.add_argument('file', help="File to append lookup")
-parser.add_argument('-c', '--csv', action='store_true', help="Output translation in CSV format")
+parser.add_argument('-a', '--anki', action='store_true', help="Output translation in Anki importable format")
 parser.add_argument('-r', '--reverse', action='store_true', help="Do reverse lookup (English to German)")
 parser.add_argument('-p', '--phrase', action='store_true', help="Phrase translation")
 
@@ -80,10 +82,6 @@ def translate_word_lingua(text: str, langpair: str, top_n: int):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: glwl [OPTION] <word>")
-        sys.exit(0)
-
     data = translate_word_lingua(args.text, "de-en", 5)
     english_word_translations = [x["l2_text"] for x in data]
     parts_of_speech = set([x["wortart"] for x in data])
@@ -97,34 +95,37 @@ if __name__ == "__main__":
         print("No translations found")
         sys.exit(0)
                 
-
     for i, d in enumerate(data):
-        try:
-            params = {
-                "german": d['l2_text'],
-                "pos": WORD_CLASSES[d['wortart']],
-                "pronunciation": "",
-                "gender": "",
-                "english": d['l2_text'],
-                "example": d['sentences'][0][0],
-                "example_translated": d['sentences'][0][1],
-            }
-            line = template.substitute(params)
-        except IndexError as e:
-            line = f"{d['l2_text']} - NO EXAMPLE SENTENCES AVAILABLE"
-        finally:
-            opts.append(line)
-            print(f"{i} → {line}")
+        if args.anki:
+            try:
+                params = {
+                    "german": d['l2_text'],
+                    "pos": WORD_CLASSES[d['wortart']],
+                    "pronunciation": "",
+                    "gender": "",
+                    "english": d['l2_text'],
+                    "example": d['sentences'][0][0],
+                    "example_translated": d['sentences'][0][1],
+                }
+                line = template.substitute(params)
+            except IndexError as e:
+                line = f"{d['l2_text']} - NO EXAMPLE SENTENCES AVAILABLE"
+            finally:
+                opts.append(line)
+                print(f"{i} → {line}")
+
+        else:
+            pprint(f"{i} → {data[i]}")
+            opts.append(data[i])
 
     # Select option
     sel = -1
     while sel < 0 or sel > len(opts):
         try:
-            sel = int(input(f"\nSelect one of the above 0-{len(data)-1}: "))
+            sel = int(input(f"\nSelect one of the above 0-{len(opts)-1}: "))
         except ValueError:
             print("Invalid selection. Please enter a number in the provided range.");
             continue
-
 
     with open(pathlib.Path(args.file), "a", encoding="utf-8") as fp:
         fp.write(f"{opts[sel]}\n")
